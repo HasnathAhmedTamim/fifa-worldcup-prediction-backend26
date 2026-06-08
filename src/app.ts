4e5731ad2b45809572f93afef45ed9200dd5ece4
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-06-09 03:06:32 +0600

    fix vercel backend serverless entry

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,20 @@
 import express from "express";
 import cors from "cors";
-import { env } from "./config/env";
-import { MatchRoutes } from "./modules/matches/match.route";
-import { AuthRoutes } from "./modules/auth/auth.route";
-import { PredictionRoutes } from "./modules/predictions/prediction.route";
-import { notFoundMiddleware } from "./middleware/notFound.middleware";
-import { errorMiddleware } from "./middleware/error.middleware";
-import { AdminRoutes } from "./modules/admin/admin.route";
-import { LeaderboardRoutes } from "./modules/leaderboard/leaderboard.route";
-
 
 export const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      process.env.CLIENT_URL || "",
-      "https://*.vercel.app",
-    ],
+    origin: ["http://localhost:3000", process.env.CLIENT_URL || ""],
     credentials: true,
   }),
 );
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "World Cup Prediction League API running",
-  });
-});
-
-app.use("/api/auth", AuthRoutes);
-app.use("/api/matches", MatchRoutes);
-app.use("/api/predictions", PredictionRoutes);
-app.use("/api/admin", AdminRoutes);
-app.use("/api/leaderboard", LeaderboardRoutes);
-
-app.use(notFoundMiddleware);
-app.use(errorMiddleware);
+// your routes here
+// app.use("/api/auth", authRoutes);
+// app.use("/api/matches", matchRoutes);
+// app.use("/api/predictions", predictionRoutes);
+// app.use("/api/leaderboard", leaderboardRoutes);
+// app.use("/api/admin", adminRoutes);
