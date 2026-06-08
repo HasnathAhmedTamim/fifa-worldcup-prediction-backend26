32abb50ad8063224dc11e3c4a90da58170f987c3
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-06-09 03:11:49 +0600

    add

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,59 @@
 import express from "express";
 import cors from "cors";
 
+import { MatchRoutes } from "./modules/matches/match.route";
+import { AuthRoutes } from "./modules/auth/auth.route";
+import { PredictionRoutes } from "./modules/predictions/prediction.route";
+import { AdminRoutes } from "./modules/admin/admin.route";
+import { LeaderboardRoutes } from "./modules/leaderboard/leaderboard.route";
+
+import { notFoundMiddleware } from "./middleware/notFound.middleware";
+import { errorMiddleware } from "./middleware/error.middleware";
+
 export const app = express();
+
+const allowedOrigins = ["http://localhost:3000", process.env.CLIENT_URL].filter(
+  Boolean,
+);
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", process.env.CLIENT_URL || ""],
+    origin: (origin, callback) => {
+      // Allow Postman, browser direct requests, server-to-server requests
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      // Allow exact frontend URL
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      // Allow Vercel preview URLs
+      if (origin.endsWith(".vercel.app")) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
   }),
 );
 
 app.use(express.json());
 
-// your routes here
-// app.use("/api/auth", authRoutes);
-// app.use("/api/matches", matchRoutes);
-// app.use("/api/predictions", predictionRoutes);
-// app.use("/api/leaderboard", leaderboardRoutes);
-// app.use("/api/admin", adminRoutes);
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "World Cup Prediction League API running",
+  });
+});
+
+app.use("/api/auth", AuthRoutes);
+app.use("/api/matches", MatchRoutes);
+app.use("/api/predictions", PredictionRoutes);
+app.use("/api/admin", AdminRoutes);
+app.use("/api/leaderboard", LeaderboardRoutes);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
